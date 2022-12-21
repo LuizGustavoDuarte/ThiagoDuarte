aeac395f21351881043645d657590b50198da079
LuizGustavoDuarteChagas <elfzero0@gmail.com>
2022-12-21 17:12:19 -0300

    teste de deploy

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ class Navbar extends Component {
         return (
             <nav class="navbar navbar-expand-lg bg-light px-5">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#"><img src="/src/assets/logo.png" alt="" width={96} /></a>
+                    <a class="navbar-brand" href="#"><img src={"/src/assets/logo.png"} alt="" width={96} /></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
